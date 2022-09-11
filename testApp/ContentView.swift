70e9db14f5f546c1ecdde4f12e98098aff35dd36
metinemredonmez <metinemredonmez@hotmail.com>
2022-09-11 23:59:44 +0300

    Initial Commit

---
+++ testApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testApp
+//
+//  Created by emre on 11.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by emre on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
